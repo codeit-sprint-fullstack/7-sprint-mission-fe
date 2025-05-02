40fa34dda6db3faae1d5afacfb0c38f569048a67
dudska12 <skadud0113@naver.com>
2025-05-02 16:37:11 +0900

    feature:로그인 시 id/pw검증 및 실패 시 모달 출력기능 추가

--- login.js
+++ login.js
@@ -1,6 +1,49 @@
 const focusOut = document.querySelectorAll('[class$="password_box"]');
 const emailFocusOut =document.querySelectorAll('[class$="email_box"]');
+const loginBuntton =document.querySelector('.login_button');
+const modalClass =document.querySelector('.login_modal');
+const modalButton =document.querySelector('.modal_button');
+const overLayPage =document.querySelector('.overlay');
+const validation =document.querySelector('.focus_line');
 
+
+
+const USER_DATA = [
+  { email: 'codeit1@codeit.com', password: "codeit101!" },
+    { email: 'codeit2@codeit.com', password: "codeit202!" },
+    { email: 'codeit3@codeit.com', password: "codeit303!" },
+    { email: 'codeit4@codeit.com', password: "codeit404!" },
+    { email: 'codeit5@codeit.com', password: "codeit505!" },
+    { email: 'codeit6@codeit.com', password: "codeit606!" },
+]
+
+//로그인 데이터확인로직
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.login_button').addEventListener('click', function () {
+    const a = document.querySelector('.validation_id').value;
+    const b = document.querySelector('.validation_pw').value;
+    const findId = USER_DATA.find(user => user.email == a && user.password == b);
+    if(findId){
+      console.log("로그인 성공");
+    }
+    else{
+      console.log("로그인 실패");
+      modalClass.style.display ="block";
+      overLayPage.style.display ="block";
+    }
+  });
+});
+
+
+modalButton.onclick = () => {  
+  modalClass.style.display ="none";
+  overLayPage.style.display ="none";
+}
+
+// loginBuntton.onclick = function(){
+//   modalClass.style.display ="block";
+//   overLayPage.style.display ="block";
+// }
 
 // 패스워드 문구 추가
 focusOut.forEach(box => {
@@ -30,7 +73,7 @@ focusOut.forEach(box => {
 emailFocusOut.forEach(box => {
   const input = box.querySelector('input');
   const targetInput = document.querySelector(".email_box");
-
+  
   input.addEventListener('input' ,(e) => {
     if(e.target.value.includes("@") != true){
       if(!box.querySelector('.input_label')){
@@ -50,7 +93,3 @@ emailFocusOut.forEach(box => {
     }
   });
 });
-
-// 내가 지금 하려고한거
-// 지금회원가입창에서 인풋아래에 라벨이 점점 늘어남 그 이유가 지정이 어딘가 잘못된거같음 
-// 또 느낀거 커밋메시지바꿔야함 아직 유효성검사가 덜됐음
