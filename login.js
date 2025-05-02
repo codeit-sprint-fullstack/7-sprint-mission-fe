88c2d0e0038d5810466d624757c68dacdc461a98
Woojin <jwj3171@gmail.com>
2025-05-02 16:16:37 +0900

    feat: Implementlogin input validity check

--- login.js
+++ login.js
@@ -2,38 +2,94 @@ const emailInput = document.getElementById("formInputEmail");
 const passwordInput = document.getElementById("formInputPassword");
 const loginButton = document.getElementById("loginButton");
 
-const emailBorder = document.getElementsByClassName("error-border");
-const emailErrorText = document.getElementsByClassName("error-text");
+const errorBorder = document.getElementsByClassName("error-border");
+const errorText = document.getElementsByClassName("error-text");
+//error-border[0] - email
+//error-border[1] - pw
+
+function validatePw() {
+  const modPwInput = passwordInput.value.trim();
+  if (modPwInput === "") {
+    //공란처리
+    errorBorder[1].style.border = "1px solid red";
+    errorText[1].textContent = "비밀번호를 입력해주세요.";
+    errorText[1].style.display = "block";
+    return false;
+  } else if (modPwInput.length < 8) {
+    //비번 길이 검사
+    errorBorder[1].style.border = "1px solid red";
+    errorText[1].textContent = "비밀번호를 8자 이상 입력해주세요";
+    errorText[1].style.display = "block";
+    return false;
+  } else if (!passwordInput.checkValidity()) {
+    //pw checkValidity 통과 실패시
+    errorBorder[1].style.border = "1px solid red";
+    errorText[1].textContent = "잘못된 비밀번호 형식입니다.";
+    errorText[1].style.display = "block";
+    return false;
+  } else {
+    //정상
+    errorBorder[1].style.border = "0px";
+    errorText[1].textContent = "";
+    errorText[1].style.display = "none";
+    return true;
+  }
+  //https://velog.io/@purplew/input-validity
+}
 
 function validateEmail() {
   const modEmailInput = emailInput.value.trim();
   if (modEmailInput === "") {
     //공란처리
-    emailBorder[0].style.border = "1px solid red";
-    emailErrorText[0].textContent = "이메일을 입력해주세요.";
-    emailErrorText[0].style.display = "block";
+    errorBorder[0].style.border = "1px solid red";
+    errorText[0].textContent = "이메일을 입력해주세요.";
+    errorText[0].style.display = "block";
+    return false;
   }
   //debounce fn으로 해결
   //  else if (modEmailInput.length <= 6) {
   //   //너무 초반부터 에러메시지 안뜨게 가리기
-  //   emailBorder[0].style.border = "0px";
-  //   emailErrorText[0].textContent = "";
-  //   emailErrorText[0].style.display = "none";
-  //} 
+  //}
   else if (!emailInput.checkValidity()) {
     //이메일 checkValidity 통과 실패시
-    emailBorder[0].style.border = "1px solid red";
-    emailErrorText[0].textContent = "잘못된 이메일 형식입니다.";
-    emailErrorText[0].style.display = "block";
+    errorBorder[0].style.border = "1px solid red";
+    errorText[0].textContent = "잘못된 이메일 형식입니다.";
+    errorText[0].style.display = "block";
+    return false;
   } else {
     //정상
-    emailBorder[0].style.border = "0px";
-    emailErrorText[0].textContent = "";
-    emailErrorText[0].style.display = "none";
+    errorBorder[0].style.border = "0px";
+    errorText[0].textContent = "";
+    errorText[0].style.display = "none";
+    return true;
+  }
+}
+function isEmailValid() {
+  const value = emailInput.value.trim();
+  return value !== "" && emailInput.checkValidity();
+}
+function isPasswordValid() {
+  const value = passwordInput.value.trim();
+  return value !== "" && value.length >= 8 && passwordInput.checkValidity();
+}
+
+function updateLoginBtnState() {
+  // if (validatePw() && validateEmail())
+  //이렇게 하면 이메일 칠때 비밀번호도 같이 검사해버림 따로 상태만 검사
+
+  if (isEmailValid() && isPasswordValid()) {
+    loginButton.disbled = false;
+    loginButton.style.backgroundColor = "#3692ff";
+    loginButton.style.cursor = "pointer";
+  } else {
+    loginButton.disabled = true;
+    loginButton.style.backgroundColor = "#9ca3af";
+    loginButton.style.cursor = "not-allowed";
   }
 }
 
-function debounce(fn, delay) {
+//debounce 계속 타이핑하고 있으면 잠시 지연시킴 = 글자치고 있을때는 검사를 안시켜서 에러메시지 안나옴
+function debounce(fn, delay = 300) {
   let timer;
   return function (...args) {
     clearTimeout(timer);
@@ -42,9 +98,59 @@ function debounce(fn, delay) {
 }
 
 const debValEmail = debounce(validateEmail, 500);
+const debValPw = debounce(validatePw, 500);
 
-emailInput.addEventListener("input", debValEmail);
-emailInput.addEventListener("blur", validateEmail);
+let eventArr = ["input", "blur"];
+const validInputs = [
+  { type: emailInput, validator: debValEmail },
+  { type: passwordInput, validator: debValPw },
+];
+
+validInputs.forEach(({ type, validator }) => {
+  eventArr.forEach((evt) => {
+    type.addEventListener(evt, () => {
+      validator();
+      updateLoginBtnState();
+    });
+  });
+});
+
+// function handleEmailEvent() {
+//   debValEmail();
+//   updateLoginBtnState();
+// }
+// function handlePasswordEvent() {
+//   debValPw();
+//   updateLoginBtnState();
+// }
+// const eventArr = ["input", "blur"];
+// eventArr.forEach((listenterEvt) => {
+//   emailInput.addEventListener(listenterEvt, handleEmailEvent);
+//   passwordInput.addEventListener(listenterEvt, handlePasswordEvent);
+// });
+
+/************************** */
+
+// // emailInput.addEventListener("input", debValEmail);
+// emailInput.addEventListener("input", () => {
+//   debValEmail();
+//   updateLoginBtnState();
+// });
+// // emailInput.addEventListener("blur", validateEmail);
+// emailInput.addEventListener("blur", () => {
+//   debValEmail();
+//   updateLoginBtnState();
+// });
+// // passwordInput.addEventListener("input", debValPw);
+// passwordInput.addEventListener("input", () => {
+//   debValPw();
+//   updateLoginBtnState();
+// });
+// // passwordInput.addEventListener("blur", validatePw);
+// passwordInput.addEventListener("blur", () => {
+//   debValPw();
+//   updateLoginBtnState();
+// });
 
 //이전 코드 삭제예정
 // function checkEmailPwValidity() {
