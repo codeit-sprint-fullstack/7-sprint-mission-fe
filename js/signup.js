2f70b8548dfce4f1a3724cc0ddb815e41b73a00c
Lee Yujin <yuwls1102@gmail.com>
2025-05-07 18:25:40 +0900

    feat: 비밀번호 확인 유효성 검사 기능 구현

--- js/signup.js
+++ js/signup.js
@@ -9,7 +9,6 @@ function showError(inputElement, errorElement, message) {
 function hideError(inputElement, errorElement) {
     inputElement.classList.remove('error');
     errorElement.classList.add('hide');
-    
 }
 
 // 문자열 길이 비교 함수 : 최소 길이 조건 확인시 사용
@@ -17,15 +16,17 @@ function inputLength(value, number) {
     return value.length < number;
 }
 
-//비밀번호 정규 표현식(소문자 + 숫자 + 특수문자 + 8자 이상)
+// 비밀번호 정규 표현식(소문자 + 숫자 + 특수문자 + 8자 이상)
 const strongPasswordReg = /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[a-z\d!@#$%^&*]{8,}$/i;
 
+// 비밀번호 유효성 검사를 위한 비밀번호 input과 에러메세지 요소
 const passwordInput = document.getElementById('user-password');
 const passwordErrorMessage = document.querySelector('[data-error-for="password"]');
 
+// 비밀번호 유효성 검사
 passwordInput.addEventListener('blur', function() {
     const password = passwordInput.value;
-
+    
     if (password === '') {
         showError(passwordInput, passwordErrorMessage, '비밀번호를 입력해주세요');
         return;
@@ -47,4 +48,25 @@ passwordInput.addEventListener('blur', function() {
       }
     
       hideError(passwordInput, passwordErrorMessage);
+});
+
+// 비밀번호확인 유효성검사를 위한 비밀번호확인 input과 에러메세지 요소
+const checkPasswordInput = document.getElementById('check-user-password');
+const checkPasswordErrorMessage = document.querySelector('[data-error-for="check-password"]');
+
+// 입력된 비밀번호의 일치 여부를 확인하는 함수
+function isMatch (password, checkPassword) {
+    return password === checkPassword;
+  }
+
+  // 비밀번호 확인 유효성 검사
+checkPasswordInput.addEventListener('blur', function() {
+    const password = passwordInput.value;
+    const checkPassword = checkPasswordInput.value;
+    
+    if (!isMatch(password, checkPassword)) {
+        showError(checkPasswordInput, checkPasswordErrorMessage, '비밀번호가 일치하지 않습니다.');
+    } else {
+        hideError(checkPasswordInput, checkPasswordErrorMessage);
+    }
 });
