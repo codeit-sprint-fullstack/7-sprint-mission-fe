c6875277d209f6e6b5b2085943111705daf8e070
dudska12 <skadud0113@naver.com>
2025-05-07 10:34:07 +0900

    feature:회원가입버튼 활성화 기능 추가

--- signup.js
+++ signup.js
@@ -1,6 +1,7 @@
 const passwordFocus = document.querySelectorAll('[class$="password_box"]');
 const emailFocus = document.querySelectorAll('[class$="email_box"]');
 const confirmFocus = document.querySelectorAll('[class$="confirm_box"]');
+const signBuntton =document.querySelector('.sign_button');
 
 
 function upDateLabel(targetBox, message) {
@@ -63,3 +64,25 @@ confirmFocus.forEach(box => {
     }
   });
 });
+
+//
+
+const updateButtonId =document.querySelector('.validation_id');
+const updateButtonPw =document.querySelector('.validation_pw');
+const updateButtonnick =document.querySelector('.validation_nick');
+const updateButtonconf =document.querySelector('.validation_conf');
+
+function updateLoginButton(){
+  if(updateButtonId.value && updateButtonPw.value && updateButtonnick.value && updateButtonconf.value){
+    signBuntton.classList.add('active');
+    signBuntton.disabled  = false;
+  }
+  else{
+    signBuntton.classList.remove('active');
+    signBuntton.disabled  = true;
+  }
+}
+updateButtonId.addEventListener('input',updateLoginButton);
+updateButtonPw.addEventListener('input',updateLoginButton);
+updateButtonnick.addEventListener('input',updateLoginButton);
+updateButtonconf.addEventListener('input',updateLoginButton);
