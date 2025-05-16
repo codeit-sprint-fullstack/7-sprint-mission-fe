d3687998469d5b8aa583bbb5a0ed2861fca3ef8b
dudska12 <skadud0113@naver.com>
2025-05-16 10:41:16 +0900

    feature:product CRUD기능 구현

---
+++ ProductService.js
@@ -0,0 +1,81 @@
+const ProductUrl = new URL('https://sprint-mission-api.vercel.app/products');
+
+
+export async function getProductList(page,pageSize,keyword) {
+  try{
+  const response = await fetch(`${ProductUrl}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`)
+
+  if(!response.ok){
+    throw new Error("에러");
+  }
+  const data = await response.json();
+  return data;
+  }
+  catch(error){
+    console.error(error);
+  }
+}
+
+export async function createProduct(name,description,price,tags,images) {
+  try{
+  const response = await fetch(`${ProductUrl}`,{
+    method: "POST",
+    body:JSON.stringify({
+      name,
+      description,
+      price,
+      tags,
+      images
+    }),
+    headers:{
+      "Content-Type" : "application/json"
+    }
+  });
+  if(!response.ok){
+    throw new Error("에러");
+  }
+  const data = await response.json();
+  return data;
+}
+catch(error){
+  console.log(error);
+}
+}
+
+export async function patchProduct(id, update) {
+  try{
+  const response = await fetch(`${ProductUrl}/${id}`,{
+    method: "PATCH",
+    body:JSON.stringify(update),
+    headers:{
+      "Content-Type" : "application/json"
+    }
+  });
+
+  if(!response.ok){
+    throw new Error("에러");
+  }
+  const data = await response.json();
+  return data;
+  }
+  catch(error){
+    console.error(error);
+  }
+}
+
+export async function deleteProduct(id) {
+  try{
+  const response = await fetch(`${ProductUrl}/${id}`,{
+    method: "DELETE",
+  });
+  if(!response.ok){
+    throw new Error("에러");
+  }
+  const data = await response.json();
+  return data;
+  }
+  catch(error){
+    console.error(error);
+  }
+
+}
