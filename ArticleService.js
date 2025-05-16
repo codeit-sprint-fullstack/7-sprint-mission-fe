1d8c86ae2634849c783032ed66df5c40570e442a
dudska12 <skadud0113@naver.com>
2025-05-16 10:40:19 +0900

    feature:articleService CRUD기능 구현

---
+++ ArticleService.js
@@ -0,0 +1,75 @@
+const articlesUrl = new URL("https://sprint-mission-api.vercel.app/articles");
+
+export function getArticleList(page, pageSize, keyword){
+  return fetch(`${articlesUrl}/?page=${page}&pageSize=${pageSize}&keyword=${keyword}`)
+  .then((response)=>{
+      if(!response.ok){
+        throw new Error("error");
+      }
+    return response.json();
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+}
+
+export function createArticle(title,content,image) {
+  return fetch(`${articlesUrl}`,{
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      content,
+      image
+    }),
+    headers: {
+      "Content-Type" : "application/json"
+      }
+  })
+  .then((response)=>{
+
+    if(!response.ok){
+      throw new Error("error");
+    }
+    return response.json();
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+  
+}
+
+export function patchArticle(id, update){
+  return fetch(`${articlesUrl}/${id}`,{
+    method: "PATCH",
+    body: JSON.stringify(update),
+    headers: {
+      "Content-Type" : "application/json"
+    }
+  })
+  .then((response)=>{
+    if(!response.ok){
+      throw new Error("에러");
+    }
+    return response.json();
+  })
+  .catch((error)=>{
+    console.error(error);
+  })
+}
+
+export function deleteArticle(id){
+    return fetch(`${articlesUrl}/${id}`,{
+    method: "DELETE",
+  })
+  .then((response)=>{
+    if(!response.ok){
+      throw new Error("에러");
+    }
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+}
+function asd(){
+  
+}
