e818a92f28ed928eb1e72e6e68e8736d55e6dc09
Lee Yujin <yuwls1102@gmail.com>
2025-05-23 10:50:43 +0900

    feat: ProductCard UI 초안 및 관련 asset

---
+++ src/components/ProductCard/index.jsx
@@ -0,0 +1,18 @@
+import favoriteIcon from "../../assets/ic_heart.svg";
+import styles from "./ProductCard.module.css";
+
+export const ProductCard = ({ src, name, price, favoriteCount }) => {
+  return (
+    <section className={styles.card}>
+      <img className={styles.productImage} src={src} alt={name} />
+      <h3 className={styles.price}>{name}</h3>
+      <p className={styles.price}>{price}원</p>
+      <div className={styles.like}>
+        <button className={styles.likeButton}>
+          <img className={styles.likeImage} src={favoriteIcon} />
+        </button>
+        <p className={styles.likeCount}>{favoriteCount}</p>
+      </div>
+    </section>
+  );
+};
