a276e343b469a2d40d0d89339207f9fcacd1fd7b
dudska12 <skadud0113@naver.com>
2025-05-29 22:00:22 +0900

    feat:네비게이션 바 컴포넌트 구현 및 반응형 디자인 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import PandaMaket from "./pages/pandaMaket";
+import Nav from "./component/nav/nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <PandaMaket></PandaMaket>
     </>
   );
--- src/component/nav/nav.jsx
+++ src/component/nav/nav.jsx
@@ -1,10 +1,41 @@
+import LogoIcon from "../../assets/image/icon_panda.svg";
+import "./nav.css";
+import { Link } from "react-router-dom";
+import LoginIcon from "../../assets/image/LoginIcon.png";
+
 const Nav = () => {
   return (
-    <div>
-      <div>{/* 로고/판다마켓 */}</div>
-      <div>{/*자유게시판  */}</div>
-      <div>{/*중고마켓 */}</div>
-      <div>{/*로그인버튼 */}</div>
+    <div className="NavTopLevel">
+      <div className="NavLeft">
+        <div className="NavLogo">
+          <Link to="/" className="LogoWrapper">
+            <img src={LogoIcon} alt="판다로고" className="LogoIcon" />
+            <span className="LogoText">판다마켓</span>
+          </Link>
+        </div>
+        <div className="NavButton">
+          <div className="NavBtnStyle">
+            <Link>
+              <span>자유게시판</span>
+            </Link>
+          </div>
+          <div className="NavBtnStyle">
+            <Link>
+              <span>중고마켓</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="NavRight">
+        <div className="NavLogin">
+          <Link>
+            <img src={LoginIcon} className="LoginIcon"></img>
+            <span className="LoginText">로그인</span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
+
+export default Nav;
