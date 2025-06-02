21cd8b475be7227e7095dab14e9be4c260deac41
Lee Yujin <yuwls1102@gmail.com>
2025-06-02 10:17:19 +0900

    feat: 임시로 루트 경로에 ProductPage 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { ProductPage } from "../src/pages/ProductPage";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProductPage } from "./pages/ProductPage";
+import { PATH } from "../utils/path.js";
 function App() {
-  return <ProductPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={PATH.index()} element={<ProductPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ utils/path.js
@@ -0,0 +1,14 @@
+export const PATH = {
+  index() {
+    return "/";
+  },
+  hotItems() {
+    return "/hotItem";
+  },
+  items() {
+    return "/items";
+  },
+  registerItem() {
+    return "/registerItem";
+  },
+};
