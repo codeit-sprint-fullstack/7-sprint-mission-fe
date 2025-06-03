a9bdd51ef02eb22fb0998554538da727c55a93f0
Woojin <jwj3171@gmail.com>
2025-06-03 10:19:16 +0900

    feat(productCard): Implement image placeholder

--- my-app/src/components/productCard.jsx
+++ my-app/src/components/productCard.jsx
@@ -2,19 +2,24 @@ import styles from "./productCard.module.css";
 import productPlaceHolder from "../assets/landscape-placeholder-svgrepo-com.svg";
 
 const ProductCard = ({ product }) => {
-  const {name,price,images,favoriteCount} = product;
-  const imgUrl =
-    images &&images.length > 0
-      ? images[0]
-      : productPlaceHolder;
+  const { name, price, images, favoriteCount } = product;
+  const imgUrl = images && images.length > 0 ? images[0] : productPlaceHolder;
 
+  const handleXBoxImage = (e) => {
+    e.target.src = productPlaceHolder;
+  };
   return (
     <div className={styles.card}>
-      <img src={imgUrl} alt={name} className={styles.image} />
+      <img
+        src={imgUrl}
+        alt={name}
+        className={styles.image}
+        onError={handleXBoxImage}
+      />
       <div className={styles.info}>
         <h3 className={styles.title}>{name}</h3>
         <p className={styles.price}>{price.toLocaleString()}원</p>
-        <p className={styles.likes}>❤️ {favoriteCount??0}</p>
+        <p className={styles.likes}>❤️ {favoriteCount ?? 0}</p>
       </div>
     </div>
   );
