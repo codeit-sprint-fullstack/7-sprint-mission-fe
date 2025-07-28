560fb202e142f3f118151fb35d88a723ffa6df10
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-07-28 17:18:04 +0900

    비밀번호 입력란 visible 아이콘 추가

--- components/CustomInput.js
+++ components/CustomInput.js
@@ -1,20 +1,33 @@
-import InputBox from "./InputBox";
+import Image from "next/image";
 import { checkBlank } from "@/utils/validInput";
 import styles from "./CustomInput.module.css";
+import { useState } from "react";
 
 export default function CustomInput({ object, password }) {
   const { element, checkValid, invalidText } = object;
+  const [isVisible, setIsVisible] = useState(false);
 
   const isValid = password ? !checkValid(password) : !checkValid();
-  const handleChange = (e) => {
-    const value = e.target.value;
-    object.setElement(value);
-  };
-
   const inputStyles =
     isValid && !checkBlank(element)
       ? `${styles.input} ${styles.invalid}`
       : styles.input;
+  const inputType = object.secret && !isVisible ? "password" : "";
+
+  const IcVisible = isVisible
+    ? "btn_visibility_on_24px.svg"
+    : "btn_visibility_off_24px.svg";
+
+  const onVisible = () => {
+    setIsVisible((prev) => {
+      return !prev;
+    });
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    object.setElement(value);
+  };
 
   return (
     <div className={styles.customInput}>
@@ -24,7 +37,18 @@ export default function CustomInput({ object, password }) {
         placeholder={object.placeholderText}
         value={object.element}
         onChange={handleChange}
+        type={inputType}
       />
+      {object.secret && (
+        <Image
+          className={styles.icVisible}
+          src={IcVisible}
+          width={24}
+          height={24}
+          alt="비밀번호 숨기기"
+          onClick={onVisible}
+        />
+      )}
       {isValid && !checkBlank(element) && (
         <div className={styles.invalidText}>{invalidText}</div>
       )}
