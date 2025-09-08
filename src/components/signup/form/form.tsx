7eebdd9c7ed64525bbb95984fd5992b31eb769df
dudska12 <skadud0113@naver.com>
2025-09-08 10:25:16 +0900

    회원가입 로그인 css 작업완료

--- src/components/signup/form/form.tsx
+++ src/components/signup/form/form.tsx
@@ -1,17 +1,22 @@
 import style from "./form.module.css";
 function Form() {
   return (
-    <form className={style.Container}>
-      <label>이메일</label>
-      <input placeholder="이메일을 입력해주세요"></input>
-      <label>닉네임</label>
-      <input placeholder="닉네임을 입력해주세요"></input>
-      <label>비밀번호</label>
-      <input placeholder="비밀번호를 입력해주세요"></input>
-      <label>비밀번호 확인</label>
-      <input placeholder="비밀번호를 다시 한 번 입력해주세요"></input>
-      <button className={style.FormButton}>회원가입</button>
-    </form>
+    <div>
+      <form className={style.Container}>
+        <label>이메일</label>
+        <input placeholder="이메일을 입력해주세요"></input>
+        <label>닉네임</label>
+        <input placeholder="닉네임을 입력해주세요"></input>
+        <label>비밀번호</label>
+        <input type="password" placeholder="비밀번호를 입력해주세요"></input>
+        <label>비밀번호 확인</label>
+        <input
+          type="password"
+          placeholder="비밀번호를 다시 한 번 입력해주세요"
+        ></input>
+        <button className={style.FormButton}>회원가입</button>
+      </form>
+    </div>
   );
 }
 
